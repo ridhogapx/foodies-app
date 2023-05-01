8510db4cec6d682ffb95418233be0184d1033977
RageNeko26 <rneko2006@gmail.com>
2023-05-01 20:31:12 +0700

    Fix attribute in app-bar component

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -2,6 +2,7 @@ import img from '../img/icon.png';
 
 class AppBar extends HTMLElement {
 	connectedCallback() {
+		this.caption = this.getAttribute('caption');
 		this.render();
 	}
 
@@ -10,7 +11,7 @@ class AppBar extends HTMLElement {
 			<nav class="navbar navbar-expand-lg  navbar-dark" style="background: #23180d;">
 			  <div class="container">
 			    <a class="navbar-brand " href="#" style="color: #F97B22;">
-			    <img src="${img}"> Foodies App</a>
+			    <img src="${img}" class="me-2">${this.caption}</a>
 			  </div>
 			</nav>
 		`
