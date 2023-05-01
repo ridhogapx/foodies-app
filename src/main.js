492d4e2caeafc12afee8d31c215259519f033d84
RageNeko26 <rneko2006@gmail.com>
2023-05-01 23:28:04 +0700

    HTTP Request using Axios

--- src/main.js
+++ src/main.js
@@ -3,15 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 
-
-
+// Components
 import './components/AppBar.js';
 import './components/SearchBar.js';
 
+// Axios as HTTP Request
 import axios from 'axios';
 
 
 axios.get('https://www.themealdb.com/api/json/v1/1/search.php?f=c')
 	.then(result => {
-		console.log(result);
+		let response = result.data.meals;
+		for(let i = 0; i < response.length; i++ ) {
+			console.log(response[i].strMeal);
+		}
+	}).catch(err => {
+		console.log(err);
 	})
