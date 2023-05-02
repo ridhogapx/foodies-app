d39c4097805a9847ae63adf35801ecf200916921
RageNeko26 <rneko2006@gmail.com>
2023-05-02 08:18:25 +0700

    Adding food container as container item data food

---
+++ src/components/FoodContainer.js
@@ -0,0 +1,19 @@
+import './FoodContent.js';
+
+class FoodContainer extends HTMLElement {
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		this.innerHTML = `
+			<div class="container">
+				<div class="row">
+					<div id="item"></div>
+				</div>
+			</div>
+		`
+	}
+}
+
+customElements.define('food-container', FoodContainer);
